d03f16e2938ceeb3d891ac89ce6edda987e06724
christorra93 <christorra93@gmail.com>
2023-09-19 01:26:50 -0300

    Created basic functions to operate two numbers

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mult(a,b){
+    return a*b;
+}
+
+function divi(a,b){
+    return a/b;
+}
+
+let a = 10;
+let b = 3;
+
+console.log(add(a,b));
+console.log(sub(a,b));
+console.log(mult(a,b));
+console.log(divi(a,b));
